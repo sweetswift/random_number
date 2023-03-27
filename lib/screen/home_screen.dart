4c03eca38d4c67cb2c62c89c547c5d0755c1a6dc
LeeChungWoo <ios.lllccww@gmail.com>
2023-03-27 23:16:11 +0900

    Add: 타이틀,숫자값,버튼 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -10,6 +10,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('랜덤숫자 생성기'),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("123"),
+              Text("456"),
+              Text("789"),
+            ],
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('생성하기!'),
+          ),
+        ),
+      ],
+    ));
   }
 }
