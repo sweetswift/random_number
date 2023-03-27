9432db6f6a24892a535e61f0f14bd210b78984e4
LeeChungWoo <ios.lllccww@gmail.com>
2023-03-27 23:52:09 +0900

    Modify: SafeArea적용,전체 좌우 패딩적용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,51 +12,56 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: PRIMARY_COLOR,
-        body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              '랜덤숫자 생성기',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.w700,
-              ),
+        backgroundColor: PRIMARY_COLOR,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 16.0,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.settings,
-                color: RED_COLOR,
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '랜덤숫자 생성기',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: RED_COLOR,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("123"),
+                      Text("456"),
+                      Text("789"),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+                    onPressed: () {},
+                    child: Text('생성하기!'),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("123"),
-              Text("456"),
-              Text("789"),
-            ],
           ),
-        ),
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: RED_COLOR
-            ),
-            onPressed: () {},
-            child: Text('생성하기!'),
-          ),
-        ),
-      ],
-    ));
+        ));
   }
 }
