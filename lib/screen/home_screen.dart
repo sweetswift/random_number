d22278705a7738b65a2ff1f84aa87ab4da10a7fa
LeeChungWoo <ios.lllccww@gmail.com>
2023-03-28 23:49:02 +0900

    Modify: 코드정리

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -20,88 +20,121 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: PRIMARY_COLOR,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '랜덤숫자 생성기',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.settings,
-                        color: RED_COLOR,
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: randomNumbers
-                          .asMap()
-                          .entries
-                          .map(
-                            (x) => Padding(
-                              padding: EdgeInsets.only(
-                                  bottom: x.key == 2 ? 0 : 16.0),
-                              child: Row(
-                                children: x.value
-                                    .toString()
-                                    .split('')
-                                    .map(
-                                      (y) => Image.asset(
-                                        'asset/img/$y.png',
-                                        height: 70.0,
-                                        width: 50.0,
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            ),
-                          )
-                          .toList()),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
-                    onPressed: () {
-                      final rand = Random();
-                      final Set<int> newNumbers = {};
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _Header(),
+              _Body(randomNumbers: randomNumbers),
+              _Footer(onPressed: onRandomNumberGenerate),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-                      while(newNumbers.length !=3) {
-                        final number = rand.nextInt(1000);
+  onRandomNumberGenerate() {
+    final rand = Random();
+    final Set<int> newNumbers = {};
 
-                        newNumbers.add(number);
-                      }
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(1000);
 
-                      setState(() {
-                        randomNumbers = newNumbers.toList();
-                      });
+      newNumbers.add(number);
+    }
 
-                      print(rand.nextInt(1000));
-                    },
-                    child: Text('생성하기!'),
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+
+    print(rand.nextInt(1000));
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.settings,
+            color: RED_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: randomNumbers
+              .asMap()
+              .entries
+              .map(
+                (x) => Padding(
+                  padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                  child: Row(
+                    children: x.value
+                        .toString()
+                        .split('')
+                        .map(
+                          (y) => Image.asset(
+                            'asset/img/$y.png',
+                            height: 70.0,
+                            width: 50.0,
+                          ),
+                        )
+                        .toList(),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ));
+              )
+              .toList()),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+        onPressed: onPressed,
+        child: Text('생성하기!'),
+      ),
+    );
   }
 }
