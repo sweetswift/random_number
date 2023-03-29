5a63e40a42401c58dda12bada3752f221afe5bfc
LeeChungWoo <ios.lllccww@gmail.com>
2023-03-29 23:01:50 +0900

    Modify: 기본ui 완성

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_number/constant/color.dart';
 
 class SettingScreen extends StatefulWidget {
   const SettingScreen({Key? key}) : super(key: key);
@@ -11,8 +12,39 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Setting Screen'),
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Row(
+                    children: 10000
+                        .toString()
+                        .split('')
+                        .map(
+                          (e) => Image.asset(
+                            'asset/img/$e.png',
+                            width: 50.0,
+                            height: 70.0,
+                          ),
+                        )
+                        .toList()),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: RED_COLOR
+                ),
+                child: Text('저장!'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
