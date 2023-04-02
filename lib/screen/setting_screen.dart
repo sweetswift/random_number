019b93618a9854c2336604feaa051a589f8c3c3e
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-02 22:23:38 +0900

    Refactor: 위젯 분리

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -23,42 +23,83 @@ class _SettingScreenState extends State<SettingScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Row(
-                    children: maxNumber
-                        .toInt()
-                        .toString()
-                        .split('')
-                        .map(
-                          (e) => Image.asset(
-                            'asset/img/$e.png',
-                            width: 50.0,
-                            height: 70.0,
-                          ),
-                        )
-                        .toList()),
-              ),
-              Slider(
-                value: maxNumber,
-                min: 1000,
-                max: 100000,
-                onChanged: (double val) {
-                  setState(() {
-                    maxNumber = val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(maxNumber.toInt());
-                },
-                style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
-                child: Text('저장!'),
+              _Body(maxNumber: maxNumber),
+              _Footer(
+                maxNumber: maxNumber,
+                onSliderChanged: onSliderChanged,
+                onButtonPressed: onButtonPressed,
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    setState(() {
+      maxNumber = val;
+    });
+  }
+
+  void onButtonPressed() {
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+          children: maxNumber
+              .toInt()
+              .toString()
+              .split('')
+              .map(
+                (e) => Image.asset(
+                  'asset/img/$e.png',
+                  width: 50.0,
+                  height: 70.0,
+                ),
+              )
+              .toList()),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final ValueChanged<double>? onSliderChanged;
+  final VoidCallback onButtonPressed;
+
+  const _Footer({
+    required this.maxNumber,
+    required this.onSliderChanged,
+    required this.onButtonPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onSliderChanged,
+        ),
+        ElevatedButton(
+          onPressed: onButtonPressed,
+          style: ElevatedButton.styleFrom(backgroundColor: RED_COLOR),
+          child: Text('저장!'),
+        ),
+      ],
     );
   }
 }
